04cdb611e92b9c9c973dee8043c4ed2f0079b711
Chackakaca <chackakaca@gmail.com>
2025-02-18 21:08:10 -0800

    added saving and upgrades

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
 var gameData = {
+    version: 0.1,
     orbs: 0,
-    orbsPerClick: 1
+    orbsPerClick: 1,
+    orbsPerClickCost: 10
   }
 
   function getOrbs() {
-    gameData.orbs += gameData.orbsPerClick
-    document.getElementById("chackOrbs").innerHTML = gameData.orbs + " Orbs Retrieved"
+    gameData.orbs += gameData.orbsPerClick //you should probably figure out how to divide this and make it NOT ugly
+    document.getElementById("chackOrbs").innerHTML = gameData.orbs + " Chack Orbs"
   }
 
+function upgradeOrbsPerClick() {
+    if (gameData.orbs >= gameData.orbsPerClickCost) {
+        gameData.orbs -= gameData.orbsPerClickCost
+        gameData.orbsPerClick += 1
+        gameData.orbsPerClickCost *= 2
+        document.getElementById("chackOrbs").innerHTML = gameData.orbs + " Chack Orbs"
+        document.getElementById("perClickUpgrade").innerHTML = "Upgrade Summoning Circle | Cost: " + gameData.orbsPerClickCost
+    }
+}
+
+var savegame = JSON.parse(localStorage.getItem("goldMinerSave"))
+if (savegame !== null) {
+  if (savegame.version == gameData.version) {
+    gameData = savegame
+  }
+  else {
+    savegame.version = gameData.version
+    gameData = savegame
+  }
+  //setup some stuff that sets values after updates
+}
+
+var saveGameLoop = window.setInterval(function() {
+    localStorage.setItem("chackIncrementalSave", JSON.stringify(gameData))
+  }, 15000)
+
+var mainGameLoop = window.setInterval(function() {
+    getOrbs()
+  }, 1000)
