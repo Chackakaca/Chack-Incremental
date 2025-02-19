7095de60e2ca32f424849a5a0f07d9dab563ee15
Chackakaca <chackakaca@gmail.com>
2025-02-18 22:04:44 -0800

    idk random bullshit go

--- main.js
+++ main.js
@@ -5,6 +5,28 @@ var gameData = {
     orbsPerClickCost: 10
   }
 
+
+
+function loadGame() {
+    var savegame = JSON.parse(localStorage.getItem("chackIncrementalSave"))
+if (savegame !== null) {
+  if (savegame.version == gameData.version) {
+    gameData = savegame
+  }
+  else {
+    savegame.version = gameData.version
+    gameData = savegame
+  }
+  //setup some stuff that sets values after updates
+}
+
+function renderNumbers() {
+    document.getElementById("chackOrbs").innerHTML = gameData.orbs + " Chack Orbs"
+    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Summoning Circle | Cost: " + gameData.orbsPerClickCost
+}
+
+}
+
   function getOrbs() {
     gameData.orbs += gameData.orbsPerClick //you should probably figure out how to divide this and make it NOT ugly
     document.getElementById("chackOrbs").innerHTML = gameData.orbs + " Chack Orbs"
@@ -24,18 +46,6 @@ function saveGame() {
     localStorage.setItem("chackIncrementalSave", JSON.stringify(gameData))
 }
 
-var savegame = JSON.parse(localStorage.getItem("chackIncrementalSave"))
-if (savegame !== null) {
-  if (savegame.version == gameData.version) {
-    gameData = savegame
-  }
-  else {
-    savegame.version = gameData.version
-    gameData = savegame
-  }
-  //setup some stuff that sets values after updates
-}
-
 var saveGameLoop = window.setInterval(function() {
     saveGame()
   }, 15000)
@@ -43,3 +53,5 @@ var saveGameLoop = window.setInterval(function() {
 var mainGameLoop = window.setInterval(function() {
     getOrbs()
   }, 1000)
+
+  loadGame()
